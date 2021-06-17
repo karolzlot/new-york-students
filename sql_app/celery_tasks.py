5ed8f9cb31856e06825b77c958ae45a9b00f3668
Karol Zlot <11590960+karolzlot@users.noreply.github.com>
2021-06-17 03:37:04 +0200

    Update choropleth chart (add labels)

--- sql_app/celery_tasks.py
+++ sql_app/celery_tasks.py
@@ -34,6 +34,7 @@ def save_chart(filters_dict,id):
 
         df2 = df.groupby(['District'])[['MATH_Level_1', 'MATH_Level_2', 'MATH_L3_and_L4']].sum().reset_index()
         df2['MATH_indicator']=(df2['MATH_Level_1']+df2['MATH_Level_2']*2+df2['MATH_L3_and_L4']*3)/(df2['MATH_Level_1']+df2['MATH_Level_2']+df2['MATH_L3_and_L4'])
+        df2['MATH_indicator_label'] = df2['MATH_indicator'].apply('<b>{:,.2f}</b>'.format)
 
         import json
         import plotly.express as px
@@ -41,26 +42,59 @@ def save_chart(filters_dict,id):
         # read the neighborhood population data into a DataFrame and load the GeoJSON data
         nycmap = json.load(open("School Districts.geojson"))
 
+        path=f"/saved_charts/fig{str(id)}.png"
+
+
+
+        # more beautiful chart, but without labels so far
+
+        # # call Plotly Express choropleth function to visualize data
+        # fig = px.choropleth_mapbox(df2,
+        #                         geojson=nycmap,
+        #                         locations="District",
+        #                         featureidkey="properties.school_dist",
+        #                         color="MATH_indicator",
+        #                         color_continuous_scale="viridis",
+        #                         mapbox_style="carto-positron",
+        #                         zoom=9, center={"lat": 40.7, "lon": -73.9},
+        #                         opacity=0.7,
+        #                         #    labels="District",
+        #                         title="District",
+        #                         hover_name="District"
+        #                         )
+        # fig.write_image(path)
+
+
+
         # call Plotly Express choropleth function to visualize data
-        fig = px.choropleth_mapbox(df2,
+        fig = px.choropleth(df2,
                                 geojson=nycmap,
                                 locations="District",
                                 featureidkey="properties.school_dist",
                                 color="MATH_indicator",
                                 color_continuous_scale="viridis",
-                                mapbox_style="carto-positron",
-                                zoom=9, center={"lat": 40.7, "lon": -73.9},
-                                opacity=0.7,
-                                #    labels="District",
-                                title="District",
+                                title="indicator is calculated per all exam participating students in each school district (after filters are applied)<br>(MATH_Level_1 + MATH_Level_2 * 2 +MATH_L3_and_L4 * 3 ) /<br> (MATH_Level_1 + MATH_Level_2 + MATH_L3_and_L4)",
                                 hover_name="District"
                                 )
-        path=f"/saved_charts/fig{str(id)}.png"
 
-        
+        fig.update_geos(fitbounds='locations')  # zoom on NYC
+
+        fig['layout'].update(margin=dict(l=20,r=30,b=30,t=60))
+
+        fig.add_scattergeo(
+        geojson=nycmap,
+        locations = df2["District"],
+        text = df2["MATH_indicator_label"],
+        textfont=dict(
+                size=10,
+                color="black",
+            ),
+        featureidkey="properties.school_dist",
+        mode = 'text') 
+
+        fig.write_image(file=path,width=960,height=540,scale=2)
 
 
-        fig.write_image(path)
         
         q = db.query(models.Chart).filter(models.Chart.id==id).one().path=path
         db.commit()
